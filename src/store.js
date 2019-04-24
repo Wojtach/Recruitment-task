a81ace4998ef4a585d593248f1ee4d307bc0a237
Wojtach1 <patrykwojtach@op.pl>
2019-04-24 09:05:23 +0200

    Fixed problem with devTools

--- src/store.js
+++ src/store.js
@@ -2,11 +2,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import ReduxThunk from 'redux-thunk'
 import { rootReducer } from './reducers/rootReducer';
 
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
+// const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
 
 export const store = createStore(
     rootReducer,
     compose(
-        applyMiddleware(ReduxThunk),
-        devTools)
+        applyMiddleware(ReduxThunk))
+    // ,devTools)
 );
